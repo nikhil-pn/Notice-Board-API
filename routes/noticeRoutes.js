2d8e23ce172d1d048ca7788b3ae7b5b8555a2922
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-28 18:15:44 +0530

    date fixxing xheck

--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const express = require("express");
+const { stringify } = require("uuid");
 const router = express.Router();
 const Notice = require("../models/noticeModels");
 
@@ -33,28 +34,32 @@ router.get("/notice/:id", async (req, res) => {
 router.post("/notice", async (req, res) => {
   try {
     const { author, message, date, likes } = req.body;
-    
-    if (!author ) {
-        return res
+
+    if (!author) {
+      return res
         .status(400)
         .json({ result: "Title Missing Please input a title " });
     }
     if (!message) {
-        return res
+      return res
         .status(400)
         .json({ result: "Title Missing Please input a title " });
     }
-    
+
+    // const dateNow = "48988"
+    // const dateString = stringify(dateNow)
+    // console.log(typeof(dateString),  "dateNow", dateString);
+
     const todo = {
-        author,
-        message,
-        date,
-        likes
+      author,
+      message,
+      date : "monday 4 stay173",
+      likes,
     };
     const createdTodo = await Notice.create(todo);
-    
+
     if (createdTodo) {
-        return res.status(201).send(createdTodo);
+      return res.status(201).send(createdTodo);
     }
     console.log("reached here");
   } catch (error) {
@@ -74,8 +79,8 @@ router.put("/notice/:id/like", async (req, res) => {
       return res.status(404).send("Todo doesn't exits in database");
     }
 
-    const {dataValues} = exitingTodo; 
-    const likeCount = dataValues.likes
+    const { dataValues } = exitingTodo;
+    const likeCount = dataValues.likes;
 
     console.log(likeCount, "data values");
 
@@ -84,7 +89,7 @@ router.put("/notice/:id/like", async (req, res) => {
         author,
         message,
         date,
-        likes: likeCount + 1 ,
+        likes: likeCount + 1,
       });
       await updatedTodo.save();
       return res.status(200).send(updatedTodo);
